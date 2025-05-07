940a458962941ba8593466f8cbe190bf70d438e3
su-anon <76511406+su-anon@users.noreply.github.com>
2025-05-07 09:59:16 +0600

    temporary checkpoint

--- app.py
+++ app.py
@@ -12,50 +12,54 @@ DB_NAME = "database.db"
 
 def username_exists(username):
     with sqlite3.connect(DB_NAME) as connection:
-        return if connection.execute("select * from user where user.username=?", (username,)).fetchall()
+        return bool(connection.execute("select * from user where user.username=?", (username,)).fetchall())
 
 def email_exists(email):
     with sqlite3.connect(DB_NAME) as connection:
-        return if connection.execute("select * from user where user.email=?", (email,)).fetchall()
+        return bool(connection.execute("select * from user where user.email=?", (email,)).fetchall())
 
-
+def create_user(username, email, fullname, password, role):
+    password_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt());
+    with sqlite3.connect(DB_NAME) as connection:
+        connection.execute("insert into user (username, email, fullname, password_hash, role, joining_date) values (?, ?, ?, ?, ?, ?)",
+                           (username, email, fullname, password_hash, role, datetime.datetime.now(),))
+    return None
 
 @app.route("/", methods=["GET"])
 def index():
     if session.get("user_id"):
         return render_template("home.html", username=session['username'])
-    else:
-        return redirect(url_for("login"))
+
+    return redirect(url_for("login"))
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method=="GET":
         return render_template("register.html")
 
-    elif request.method=="POST":
-
+    if request.method=="POST":
         username = request.form.get("username")
-        email = request.form.get("email")
-
         if username_exists(username):
             return render_template("register.html", error="Username exists!")
-        elif email_exists(email):
+
+        email = request.form.get("email")
+        if email_exists(email):
             return render_template("register.html", error="Email exists!")
-        else:
-            password = request.form.get("password")
-            password_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt());
-            fullname = request.form.get("fullname")
-            connection.execute("insert into user (username, email, fullname, password_hash, role, joining_date) values (?, ?, ?, ?, ?, ?)",
-                               (username, email, fullname, password_hash, "MEMBER", datetime.datetime.now(),))
-            return redirect(url_for("login"))
+
+        fullname = request.form.get("fullname")
+        password = request.form.get("password")
+        create_user(username, email, fullname, password, "MEMBER")
+        return redirect(url_for("login"))
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method=="GET" and session.get("username"):
-        return redirect(url_for("index", username=session.get("username")))
-    elif request.method=="GET" and not session.get("username"):
+    if request.method=="GET":
+        if session.get("username"):
+            return redirect(url_for("index", username=session.get("username")))
+
         return render_template("login.html")
-    elif request.method=="POST":
+
+    if request.method=="POST":
         username = request.form.get("username")
         password = request.form.get("password")
         with sqlite3.connect(DB_NAME) as connection:
